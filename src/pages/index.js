6b4c611253b88f71243af38d7f1a0abf611772cf
Varad J <varad.gcs@gmail.com>
2024-12-30 02:57:37 +0530

    Add delete button to files

--- src/pages/index.js
+++ src/pages/index.js
@@ -199,18 +199,18 @@ export default function FormPage() {
                         <div className="flex flex-wrap gap-4">
                           {files.map((file, index) => (
                             <div key={index} className="relative">
-                              <div className="flex items-center gap-2">
-                                <button
-                                  type="button"
-                                  className="text-red-500"
-                                  onClick={() => handleFileDelete(index)}
-                                >
+                              <div className="flex flex-col items-center gap-2">
                                 <img
                                   className="w-16 h-16 object-cover rounded-md"
                                   src={URL.createObjectURL(file)}
                                   alt={`file-preview-${index}`}
                                 />
-                                <span class="material-icons"></span>
+                                <button
+                                  type="button"
+                                  className="text-red-500"
+                                  onClick={() => handleFileDelete(index)}
+                                >
+                                <span class="material-icons">delete</span>
                                 </button>
                               </div>
                             </div>
